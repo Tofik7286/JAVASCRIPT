82c4996f4a35d0e041e4e496401290a2f91bb391
tofik <tofik.imscit21@gmail.com>
2025-11-04 14:19:23 +0530

    2 variables completed

---
+++ DAY_1/2_variables/script.js
@@ -0,0 +1,154 @@
+// The console shows the output on the screen
+// console.log("hello")
+
+// This is a single line comment
+
+/*
+This is
+a multi-line
+comment
+*/
+
+// Declaring a variable for age
+// let age;
+// age = 24
+// console.log(age)
+
+// Declaring and changing the name variable
+// let name = "Shaikh Mohammad Tofik"
+// name = "Tofik"
+// console.log(name)
+
+// Declaring a variable with a string value
+// let alpha = "lakjdslkfj sadlfkj s;dlf jaf";
+// console.log(alpha)
+
+// Declaring multiple variables for a, b, and c
+// let a;
+// let b;
+// let c;
+
+// a = 10;
+// b = 20;
+// c = 30;
+
+// Declaring and assigning values to a, b, and c
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// Declaring multiple variables in one line
+// let a,b,c;
+// a = 10;
+// b=20;
+// c=30;
+
+// Declaring a variable a and b, and then showing their values
+// let a = 10;
+// let b;
+// let c;
+
+// console.log(a,b,c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// Trying to declare the same variable twice causes an error
+// let a = 1;
+// let a = 10;
+
+// Using a block of code with different variables
+// {
+//     let age = 1;
+//     let name = "manas"
+//     console.log(age);
+//     console.log(name)
+// }
+
+// These variables are outside the block and are not affected by the block
+// console.log(age);
+// console.log("hello")
+
+// Declaring constants
+// const pi = 3.14;
+// const a = 10;
+// // a = 15;
+
+// // pi = 2;
+// console.log(pi)
+
+// Declaring a variable without value
+// let alpha;
+// console.log(alpha)
+
+// Example of two variables with similar names
+// let name = "tofik"
+// let Name = "shaikh tofik"
+// console.log(name)
+// console.log(Name)
+
+// Declaring variables with different styles
+// let name;
+// let name1;
+// let name_;
+// let name$;
+
+// let $name;
+
+// Declaring price variables and calculating total
+// let full_name;
+
+// let price1 = 499;
+// let price2 = 299;
+
+// let totalPrice = price1 + price2;
+// console.log(totalPrice)
+
+// Getting input from the user for name and last name
+// let name = prompt("Enter name");
+// let lastName = prompt("Enter last name");
+// let fullName = name + " " + lastName;
+// console.log(fullName)
+
+/*
+let a = 1
+a = "alpha"
+
+a = true
+console.log(typeof a)
+console.log(typeof(a))
+
+let a1 = "2" + 2;
+let a2 = "2" - 2
+console.log(a1, typeof a1)
+console.log(a2, typeof a2)
+
+alpha = 22;
+console.log(alpha)
+*/
+
+// Trying to print an undefined variable
+// console.log(beta)
+
+// Example of undefined and null variables
+// let name;
+// let name1 = null;
+// console.log(name, typeof name)
+// console.log(name1, typeof name1)
+
+// Using BigInt for large numbers
+// let number = BigInt(23432432);
+// console.log(number, typeof number)
+
+// Declaring a boolean variable
+// let isLoggedIn = false;
+// console.log(isLoggedIn, typeof isLoggedIn)
+
+// Converting a string to a number
+// let str = "23";
+// let num = Number(str);
+// console.log(num, typeof num)
+
+// Getting a number from the user and showing its type
+let num = Number(prompt("Enter a number"));
+console.log(num, typeof num);
